910c32c3929794ae823994db33e6684263bff71c
kailash <kailash.iitr@gmail.com>
2024-11-28 16:09:02 +0530

    First version of  Product Service using data from fake product service.

---
+++ src/main/java/com/example/productservice/services/FakeStoreProductService.java
@@ -0,0 +1,43 @@
+package com.example.productservice.services;
+
+import com.example.productservice.dtos.FakeStoreProductDto;
+import com.example.productservice.models.Category;
+import com.example.productservice.models.Product;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class FakeStoreProductService implements  ProductService{
+    RestTemplate restTemplate;
+
+    public FakeStoreProductService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        FakeStoreProductDto fakeStoreProductDto = restTemplate.getForObject("https://fakestoreapi.com/products/" + id, FakeStoreProductDto.class);
+        return convertFakeStoreProductDtoToProduct(fakeStoreProductDto);
+    }
+
+    private Product convertFakeStoreProductDtoToProduct(FakeStoreProductDto fakeStoreProductDto) {
+        if(fakeStoreProductDto == null) {
+            return null;
+        }
+
+        Product product = new Product();
+
+        // Map basic fields
+        product.setId(fakeStoreProductDto.getId());
+        product.setTitle(fakeStoreProductDto.getTitle());
+        product.setDescription(fakeStoreProductDto.getDescription());
+        product.setPrice(fakeStoreProductDto.getPrice());
+
+        // Convert category (create a new Category object)
+        Category category = new Category();
+        category.setTitle(fakeStoreProductDto.getCategory());
+        product.setCategory(category);
+
+        return product;
+    }
+}
